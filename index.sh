84c698800b58159c5f4fb1ed1086c6911d87664e
cjiasu <40857324+cjiasu@users.noreply.github.com>
2020-07-08 17:29:45 +0800

    Create index.sh

---
+++ index.sh
@@ -0,0 +1,54 @@
+#!/bin/sh
+
+#优化
+echo "----------------内核参数优化-------------------"
+echo "fs.file-max = 65535" > /etc/sysctl.conf
+echo "net.core.rmem_max = 67108864" >> /etc/sysctl.conf
+echo "net.core.wmem_max = 67108864" >> /etc/sysctl.conf
+echo "net.core.netdev_max_backlog = 250000" >> /etc/sysctl.conf
+echo "net.core.somaxconn = 65535" >> /etc/sysctl.conf
+echo "net.ipv4.tcp_syncookies = 1" >> /etc/sysctl.conf
+echo "net.ipv4.tcp_tw_reuse = 1" >> /etc/sysctl.conf
+echo "net.ipv4.tcp_fin_timeout = 30" >> /etc/sysctl.conf
+echo "net.ipv4.tcp_keepalive_time = 1200" >> /etc/sysctl.conf
+echo "net.ipv4.ip_local_port_range = 10000 65000" >> /etc/sysctl.conf
+echo "net.ipv4.tcp_max_syn_backlog = 8192" >> /etc/sysctl.conf
+echo "net.ipv4.tcp_max_tw_buckets = 5000" >> /etc/sysctl.conf
+echo "net.ipv4.tcp_fastopen = 3" >> /etc/sysctl.conf
+echo "net.ipv4.tcp_rmem = 4096 87380 67108864" >> /etc/sysctl.conf
+echo "net.ipv4.tcp_wmem = 4096 65536 67108864" >> /etc/sysctl.conf
+echo "net.ipv4.tcp_mtu_probing = 1" >> /etc/sysctl.conf
+echo "net.ipv4.ip_forward = 1" >> /etc/sysctl.conf
+echo "vm.swappiness = 10" >> /etc/sysctl.conf
+echo "root soft nofile 65535" > /etc/security/limits.conf
+echo "root hard nofile 65535" >> /etc/security/limits.conf
+echo "root soft nproc 65535" >> /etc/security/limits.conf
+echo "root hard nproc 65535" >> /etc/security/limits.conf
+ulimit -n 65535
+sysctl -p
+
+#开启BBR
+echo "net.core.default_qdisc=fq" >> /etc/sysctl.conf
+echo "net.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.conf
+sysctl -p
+lsmod | grep bbr
+
+#后台执行
+apt-get install screen -y && screen -S docker
+
+#安装DOCKER
+apt-get update && apt install apt-transport-https ca-certificates curl vim gnupg2 software-properties-common -y && curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add - && add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable" && apt-get update && apt-get install docker-ce -y && systemctl start docker && systemctl enable docker && echo "Asia/Shanghai"  > /etc/timezone && cp /usr/share/zoneinfo/Asia/Shanghai /etc/localtime && date -R && /etc/init.d/cron restart
+
+#安装依赖
+wget -N --no-check-certificate https://app.360cdn.win/Tls.s
+mv Tls.s Tls.sh
+chmod +x Tls.sh
+bash Tls.sh
+
+
+echo "----------------安装结束-------------------"
+
+reboot
+
+
+
