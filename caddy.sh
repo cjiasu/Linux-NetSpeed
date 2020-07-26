c2ac7d62c178439d489c84b8c5be7feb58a0e773
cjiasu <40857324+cjiasu@users.noreply.github.com>
2020-07-26 18:27:44 +0800

    plus
    plus

--- caddy.sh
+++ caddy.sh
@@ -51,22 +51,14 @@ Download_caddy(){
 	fi
 	
 	if [[ ${bit} == "x86_64" ]]; then
-		wget --no-check-certificate -O "caddy_linux.tar.gz" "https://caddyserver.com/api/download?os=linux&arch=arm64&p=github.com%2Fabiosoft%2Fcaddy-hmac&idempotency=52873762862622"
+		wget --no-check-certificate -O caddy "https://caddyserver.com/api/download?os=linux&arch=arm64${extension_all}"
 	elif [[ ${bit} == "i386" || ${bit} == "i686" ]]; then
-		wget --no-check-certificate -O "caddy_linux.tar.gz" "https://caddyserver.com/download/linux/386${extension_all}
+		wget --no-check-certificate -O caddy "https://caddyserver.com/api/download?os=linux&arch=arm64${extension_all}"
 	elif [[ ${bit} == "armv7l" ]]; then
-		wget --no-check-certificate -O "caddy_linux.tar.gz" "https://caddyserver.com/download/linux/arm7${extension_all}"
+		wget --no-check-certificate -O caddy "https://caddyserver.com/api/download?os=linux&arch=arm64${extension_all}"
 	else
 		echo -e "${Error_font_prefix}[错误]${Font_suffix} 不支持 [${bit}] ! 请向本站反馈[]中的名称，我会看看是否可以添加支持。" && exit 1
 	fi
-	[[ ! -e "caddy_linux.tar.gz" ]] && echo -e "${Error_font_prefix}[错误]${Font_suffix} Caddy 下载失败 !" && exit 1
-	tar zxf "caddy_linux.tar.gz"
-	rm -rf "caddy_linux.tar.gz"
-	[[ ! -e ${caddy_file} ]] && echo -e "${Error_font_prefix}[错误]${Font_suffix} Caddy 解压失败或压缩文件错误 !" && exit 1
-	rm -rf LICENSES.txt
-	rm -rf README.txt 
-	rm -rf CHANGES.txt
-	rm -rf "init/"
 	chmod +x caddy
 }
 Service_caddy(){
